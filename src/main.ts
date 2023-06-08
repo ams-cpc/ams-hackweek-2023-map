cefd009ce2177c7c1862673a398796726a106590
Faisal Ahmed <faisalg2006@gmail.com>
2023-06-08 15:37:14 +0200

    Updates to script & fashion shop

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,14 @@ const handleAd = () => {
     WA.room.area.onEnter("nikead").subscribe(() => {});
 }
 
+const handleFashionShop = () => {
+    WA.room.area.onEnter('entryShop').subscribe(() => {
+        currentPopup = WA.ui.openPopup("welcomeNote", "Welcome to Top Fashion shop where fashion dreams meet reality", []);
+    })
+    WA.room.area.onLeave("entryShop").subscribe(closePopup);
 
+
+}
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
@@ -56,6 +63,7 @@ WA.onInit().then(() => {
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
     handleAd();
+    handleFashionShop();
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
