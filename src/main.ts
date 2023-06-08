99d51c93c75162d212fbafebd8f5e307cf24f224
Ozan Incesulu <oincesulu@ebay.com>
2023-06-08 12:24:02 +0200

    Merged all into one, except Pokemon

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,51 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import {Popup} from "@workadventure/iframe-api-typings";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
+let currentPopup: Popup | undefined = undefined;
+const handleAd = () => {
+    WA.room.area.onEnter("airjordanad").subscribe(() => {
+        currentPopup = WA.ui.openPopup("airjordanadcontent", "The shoes are in 30% discount!", [
+            {
+                label: "For Men",
+                className: "success",
+                callback: (popup) => {
+                    popup.close();
+                    WA.ui.modal.openModal({
+                        title: "Jordan Sneakers for Men",
+                        src: "https://www.ebay.com/b/Jordan-Sneakers-for-Men/15709/bn_96541848",
+                        allow: "fullscreen",
+                        allowApi: false,
+                        position: "center"
+                    });
+                }
+            }, {
+                label: "For Women",
+                className: "warning",
+                callback: (popup) => {
+                    popup.close();
+                    WA.ui.modal.openModal({
+                        title: "Jordan Sneakers for Women",
+                        src: "https://www.ebay.com/b/Jordan-Sneakers-for-Women/95672/bn_80975750",
+                        allow: "fullscreen",
+                        allowApi: false,
+                        position: "center"
+                    });
+                }
+            }
+        ]);
+        WA.room.area.onLeave("airjordanad").subscribe(closePopup);
+    });
+}
+
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
-
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
         const time = today.getHours() + ":" + today.getMinutes();
@@ -18,7 +53,7 @@ WA.onInit().then(() => {
     })
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
-
+    handleAd();
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
